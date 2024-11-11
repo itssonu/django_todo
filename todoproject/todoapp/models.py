7e32e0ae5bcea869a37d831e7cc210940750c85f
sonutechbit <sonu.techbit@outlook.com>
2024-11-11 13:36:00 +0530

    migration done

--- todoproject/todoapp/models.py
+++ todoproject/todoapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
